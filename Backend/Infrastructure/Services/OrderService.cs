2f91d36eb9800ace971155aa95ec55e97a6d6972
heni06@msn.com <heni06@msn.com>
2023-10-10 23:42:44 +0200

    AddOrUpdate Customer and Order Mutation

--- Backend/Infrastructure/Services/OrderService.cs
+++ Backend/Infrastructure/Services/OrderService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Core.Entities;
 using Core.Interfaces;
+using Core.Models;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,6 +18,8 @@ namespace Infrastructure.Services
         {
             _dbContextFactory = dbContextFactory;
         }
+
+
         public IQueryable<Order> GetOrders()
         {
             var context = _dbContextFactory.CreateDbContext();
@@ -25,6 +28,57 @@ namespace Infrastructure.Services
             return context.Orders
                 .Where(o => !o.IsDeleted)
                 .Include(o => o.Customer);
+        }
+
+        public async Task<Order> AddOrUpdateOrderAsync(OrderModel orderModel)
+        {
+            var context = _dbContextFactory.CreateDbContext();
+            Order order;
+
+            var customer = await context.Customers
+                            .Where(c => c.Id == orderModel.CustomerId)
+                            .FirstOrDefaultAsync();
+            if(customer == null){
+                throw new Exception($"Customer with id {orderModel.CustomerId} was not found");
+            }
+            if(orderModel.Id ==null){
+                order = new Order {
+                    CustomerId = orderModel.CustomerId,
+                    OrderDate = orderModel.OrderDate,
+                    Description = orderModel.Description,
+                    Totalmount = orderModel.Totalmount,
+                    DepositAmount = orderModel.DepositAmount,
+                    IsDelivery = orderModel.IsDelivery,
+                    Status = orderModel.Status,
+                    OtherNotes = orderModel.OtherNotes   
+                };
+                
+                await context.Orders.AddAsync(order); 
+                
+            }
+            else{
+                order = await context.Orders
+                            .Where(o => o.Id == orderModel.Id)
+                            .FirstOrDefaultAsync();
+
+                if(order == null){
+                    throw new Exception($"Order with id {orderModel.Id} was not found");
+                }
+
+                order.OrderDate = orderModel.OrderDate;
+                order.Description = orderModel.Description;
+                order.Totalmount = orderModel.Totalmount;
+                order.DepositAmount = orderModel.DepositAmount;
+                order.IsDelivery = orderModel.IsDelivery;
+                order.Status = orderModel.Status;
+                order.OtherNotes = orderModel.OtherNotes;
+
+                context.Orders.Update(order);
+
+            }
+            await context.SaveChangesAsync();
+
+            return order;
         }
     }
 }
